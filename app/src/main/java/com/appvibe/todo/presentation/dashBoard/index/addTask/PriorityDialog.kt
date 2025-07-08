ba5458a5feec37daf499959965fe7cea1100f383
Muhammad Farhan <muhammadf@joblogic.com>
2025-07-08 19:32:45 +0500

    Updated the Item

--- app/src/main/java/com/appvibe/todo/presentation/dashBoard/index/addTask/PriorityDialog.kt
+++ app/src/main/java/com/appvibe/todo/presentation/dashBoard/index/addTask/PriorityDialog.kt
@@ -1,13 +1,24 @@
 package com.appvibe.todo.presentation.dashBoard.index.addTask
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.lazy.grid.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.window.Dialog
+import com.appvibe.todo.R
+import com.appvibe.todo.ui.theme.TODOTheme
+import com.appvibe.todo.ui.theme.White
 
 @Composable
 fun TaskPriorityDialog(
@@ -23,17 +34,30 @@ fun TaskPriorityDialog(
             color = Color(0xFF2B2B2B) // dark theme background
         ) {
             Column(modifier = Modifier.padding(16.dp)) {
-                Text("Task Priority", style = MaterialTheme.typography.titleMedium)
+                Text(
+                    "Task Priority",
+                    style = MaterialTheme.typography.titleMedium,
+                    color = MaterialTheme.colorScheme.onBackground,
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = androidx.compose.ui.text.style.TextAlign.Center
+                )
 
                 Spacer(Modifier.height(8.dp))
+
+                HorizontalDivider(
+                    modifier = Modifier.fillMaxWidth(),
+                    thickness = 1.dp,
+                    color = White
+                )
+
+                Spacer(Modifier.height(15.dp))
 
                 LazyVerticalGrid(
                     columns = GridCells.Fixed(4),
                     modifier = Modifier
-                        .height(200.dp)
                         .fillMaxWidth(),
-                    horizontalArrangement = Arrangement.spacedBy(8.dp),
-                    verticalArrangement = Arrangement.spacedBy(8.dp)
+                    horizontalArrangement = Arrangement.spacedBy(12.dp),
+                    verticalArrangement = Arrangement.spacedBy(12.dp)
                 ) {
                     items(10) { index ->
                         val number = index + 1
@@ -44,9 +68,27 @@ fun TaskPriorityDialog(
                             colors = ButtonDefaults.buttonColors(
                                 containerColor = if (isSelected) Color(0xFF9286F7) else Color.DarkGray
                             ),
+                            shape = RoundedCornerShape(3.dp),
                             modifier = Modifier.size(64.dp)
                         ) {
-                            Text("$number")
+                            Column(
+                                modifier = Modifier.fillMaxSize()
+                            ) {
+                                Image(
+                                    painter = painterResource(R.drawable.ic_flag),
+                                    contentDescription = "Timer",
+                                    modifier = Modifier.fillMaxWidth().height(30.dp),
+                                    contentScale = androidx.compose.ui.layout.ContentScale.FillBounds,
+                                    alignment = Alignment.TopCenter,
+                                )
+                                Text(
+                                    "$number", style = MaterialTheme.typography.labelSmall,
+                                    color = MaterialTheme.colorScheme.onBackground,
+                                    modifier = Modifier.fillMaxWidth(),
+                                    textAlign = androidx.compose.ui.text.style.TextAlign.Center
+                                )
+                            }
+
                         }
                     }
                 }
@@ -72,6 +114,14 @@ fun TaskPriorityDialog(
     }
 }
 
+@Preview
+@Composable
+fun TaskPriorityDialogPreview() {
+    TODOTheme {
+        TaskPriorityDialog(onDismiss = {}, onSave = {})
+    }
+
+}
 
 
 
